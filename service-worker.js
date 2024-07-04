661d02680b11d51f1a11bd4aa3f409737721700e
Shihan Rishad <165176498+ShihanRishad@users.noreply.github.com>
2024-07-04 20:08:09 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,36 +10,18 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(keyList.map(key => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  return self.clients.claim();
-});
-
-// Listen for the 'message' event to trigger an update check
-self.addEventListener('message', event => {
-  if (event.data === 'checkForUpdate') {
-    self.skipWaiting();
-  }
 });
