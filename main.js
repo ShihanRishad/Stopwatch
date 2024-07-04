fdc32ca0d3b9b5585f7053a04f8ece0ec945fc88
Shihan Rishad <165176498+ShihanRishad@users.noreply.github.com>
2024-07-04 20:09:33 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/test123test/service-worker.js').then(registration => {
+    navigator.serviceWorker.register('/Stopwatch/service-worker.js').then(registration => {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, error => {
       console.log('ServiceWorker registration failed: ', error);
